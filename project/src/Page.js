9d860965c9405248202788320e69b932ab2c5b18
Dheepaganesh <dheepaganeshp3052dell@gmail.com>
2023-08-10 06:25:20 +0530

    Update

---
+++ project/src/Page.js
@@ -0,0 +1,21 @@
+const Page = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <a href="">Icon</a>
+        </div>
+        <div>
+          <a href="">Home</a>
+        </div>
+        <div>
+          <a href="">Pages</a>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Page;
